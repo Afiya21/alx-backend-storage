d68e31457e7bd9b331ff32a28edbfeeae6c2a30f
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-06-30 06:00:54 -0700

    my second brother fixed this one

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, IF(split is NULL , 2020 - formed, (split - formed))  lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

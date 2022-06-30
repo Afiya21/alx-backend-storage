6cb13333897f135b61703fef61596dc922b5150f
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-06-30 05:57:10 -0700

    my brother fixed it for me

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,4 @@
--- lists all bands with Glam as their main style
--- ranked by their longevity
--- Column names: band_name and lifespan (in years)
--- attributes formed and split for computing the lifespan
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

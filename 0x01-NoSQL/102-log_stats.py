f4b7ef6b441cc671cc9289f3bbd8f6f3347727a8
Afiya21 <88346482+Afiya21@users.noreply.github.com>
2022-07-05 23:17:53 -0700

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,48 +1,42 @@
 #!/usr/bin/env python3
-"""Defines a function that  provides some stats
-   about Nginx logs stored in MongoDB
-"""
+""" Script to gather log summary from nginx logs """
 
-from pymongo import MongoClient
+if __name__ == '__main__':
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    the_logs = client.logs.nginx
 
+    total = the_logs.count_documents({})
+    get = the_logs.count_documents({"method": "GET"})
+    post = the_logs.count_documents({"method": "POST"})
+    put = the_logs.count_documents({"method": "PUT"})
+    patch = the_logs.count_documents({"method": "PATCH"})
+    delete = the_logs.count_documents({"method": "DELETE"})
+    status = the_logs.count_documents({"path": "/status"})
+    ip_list = [
+            "172.31.63.67", "172.31.2.14", "172.31.29.194",
+            "69.162.124.230", "64.124.26.109", "64.62.224.29",
+            "34.207.121.61", "47.88.100.4", "45.249.84.250",
+            "216.244.66.228"
+            ]
+    ip_dict = {ip: the_logs.count_documents({"ip": ip}) for ip in ip_list}
 
-def nginx_stats_check():
-    """ provides some stats about Nginx logs stored in MongoDB:"""
-    client = MongoClient()
-    collec_nginx = client.logs.nginx
-
-    num_of_docs = collec_nginx.count_documents({})
-    print("{} logs".format(num_of_docs))
-    print("Methods:")
-    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods_list:
-        method_count = collec_nginx.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = collec_nginx.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
-
+    print(f"{total} logs")
+    print(f"Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{status} status check")
     print("IPs:")
-
-    top_IPs = collec_nginx.aggregate([
-        {"$group":
-         {
-             "_id": "$ip",
-             "count": {"$sum": 1}
-         }
-         },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ])
-    for top_ip in top_IPs:
-        count = top_ip.get("count")
-        ip_address = top_ip.get("ip")
-        print("\t{}: {}".format(ip_address, count))
-
-
-if __name__ == "__main__":
-    nginx_stats_check()
+    print(f"\t172.31.63.67: {ip_dict.get('172.31.63.67')}")
+    print(f"\t172.31.2.14: {ip_dict.get('172.31.2.14')}")
+    print(f"\t172.31.29.194: {ip_dict.get('172.31.29.194')}")
+    print(f"\t69.162.124.230: {ip_dict.get('69.162.124.230')}")
+    print(f"\t64.124.26.109: {ip_dict.get('64.124.26.109')}")
+    print(f"\t64.62.224.29: {ip_dict.get('64.62.224.29')}")
+    print(f"\t34.207.121.61: {ip_dict.get('34.207.121.61')}")
+    print(f"\t47.88.100.4: {ip_dict.get('47.88.100.4')}")
+    print(f"\t45.249.84.250: {ip_dict.get('45.249.84.250')}")
+    print(f"\t216.244.66.228: {ip_dict.get('216.244.66.228')}")
